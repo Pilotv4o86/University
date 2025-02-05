b8c0568fba752b534f24f57bce568215629a70fd
Pilot__v4o86 <sashabogdanov759@gmail.com>
2025-02-05 08:25:56 +0300

    Minor commit

--- ViewService/src/main/java/com/example/viewservice/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.viewservice.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
